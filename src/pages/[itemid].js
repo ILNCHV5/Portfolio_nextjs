4c980f6b5a7c7a6d76d6786e2f180ff94bead39f
ILNCHV5 <112627416+ILNCHV5@users.noreply.github.com>
2024-09-16 12:35:44 +0300

    added favicon, gallery item page done

--- src/pages/[itemid].js
+++ src/pages/[itemid].js
@@ -1,11 +1,10 @@
-import Box from "@mui/material/Box";
+import { Box, Typography, Button } from "@mui/material";
+import Link from "next/link";
 import styles from "@/styles/Item.module.css";
-import Typography from "@mui/material/Typography";
 import images from "/imageList.json";
-import { Inter } from "next/font/google";
-import BackBox from "../../public/components/BackBox/BackBox";
-
-const inter = Inter({ subsets: ["latin"] });
+import Header from "../../public/components/Header/Header";
+import Footer from "../../public/components/Footer/Footer";
+import VideoBackground from "../../public/components/VideoBackground/VideoBackground";
 
 export async function getStaticPaths() {
   const paths = images.map((image) => ({ params: { itemid: image.slug } }));
@@ -21,34 +20,40 @@ const ItemPage = ({ image }) => {
   const { coverImage, media, description, title } = image;
 
   return (
-    <Box className={styles.container} sx={{ px: { md: "0%", lg: "20%" } }}>
-      <Box sx={{ alignSelf: "flex-start" }}>
-        <BackBox />
-      </Box>
+    <Box
+      className={styles.container}
+      sx={{ maxWidth: { md: "70%", lg: "60%" }, mx: "auto" }}
+    >
+      <Header />
       <Box
         className={styles.typeContainer}
         sx={{ width: { xs: "90%", md: "80%", lg: "70%" } }}
       >
-        <Typography className={`${styles.title} ${inter.className}`}>
+        <Typography
+          className={styles.title}
+          sx={{ paddingTop: { xs: "4rem", md: "7rem" } }}
+        >
           {title}
         </Typography>
-        <Typography className={`${styles.typography} ${inter.className}`}>
-          {description}
-        </Typography>
+        <Typography className={styles.typography}>{description}</Typography>
       </Box>
-      <Box>
+      <Box className={styles.imageContainer}>
         <img src={coverImage} alt="Cover" style={{ width: "100%" }} />
       </Box>
       {media.map((mediaItem) => (
-        <Box key={mediaItem.id}>
+        <Box key={mediaItem.id} className={styles.imageContainer}>
           <img
             src={mediaItem.src}
-            alt={`Media ${mediaItem.id}`}
+            alt={mediaItem.id}
             style={{ width: "100%", margin: 0, padding: 0 }}
           />
         </Box>
       ))}
-      <BackBox />
+      <Link href="/" passHref>
+        <Button className={styles.button}>Back</Button>
+      </Link>
+      <Footer />
+      <VideoBackground />
     </Box>
   );
 };
